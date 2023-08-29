5dda259ee7244e3a64b2f30c59f16cf3c5e4eab6
Evan Kahn <ekahn@hmc.edu>
2023-08-28 20:13:43 -0400

    Test yosys PLL generation

--- blink/blink.sv
+++ blink/blink.sv
@@ -1,22 +1,35 @@
 module blink
-  #(parameter g_COUNT_10HZ = 25000000)
-  (input  clk_i,
+
+  #(parameter main_count_1HZ   =  25000000, // Divider to make each clock blink LED at 1Hz
+    parameter core_count_1HZ   = 180000000, 
+    parameter spdif_count_1HZ  = 196666666)
+
+  (input  clk_25m_i,
    output reg led_o = 1'b0);
+
+	pll_spdif pll_spdif (
+		.inclk0				( clk_25m_i ),
+		.c0					( clk_spdif )
+	);
+
+	pll_core pll_core (
+		.inclk0				( clk_25m_i ),
+		.c0					( clk_core )
+	);
+
+
+
+  reg [64:0] r_Count_1Hz = 0;
   
-  // These signals will be the counters:
-  reg [31:0] r_Count_10Hz = 0;
-
-  // All processes toggle a specific signal at a different frequency.
-  // They all run continuously
-
-  always @(posedge clk_i)
+  always @(posedge clk_spdif)
   begin
-    if (r_Count_10Hz == g_COUNT_10HZ)
+    if (r_Count_1Hz == spdif_count_1HZ)
     begin
-      led_o      <= ~led_o;
-      r_Count_10Hz <= 0;
+      led_o       <= ~led_o;
+      r_Count_1Hz <= 0;
     end
     else
-      r_Count_10Hz <= r_Count_10Hz + 1;
+      r_Count_1Hz <= r_Count_1Hz + 1;
   end
+
 endmodule
